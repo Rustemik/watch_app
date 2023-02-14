36f8dd995614181a5f598024412193eff48b448a
Rustemik <r132420@gmail.com>
2023-02-14 10:35:23 +0300

    + Изменения в clock.dart (стрелки)

--- lib/widgets/clock.dart
+++ lib/widgets/clock.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -10,6 +11,14 @@ class Clock extends StatefulWidget {
 }
 
 class _ClockState extends State<Clock> {
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -71,10 +80,13 @@ class _ClockState extends State<Clock> {
               ],
             ),
           ),
-          Container(
-            constraints: BoxConstraints.expand(),
-            child: CustomPaint(
-              painter: ClockPainter(),
+          Transform.rotate(
+            angle: pi / 2,
+            child: Container(
+              constraints: BoxConstraints.expand(),
+              child: CustomPaint(
+                painter: ClockPainter(),
+              ),
             ),
           ),
         ],
@@ -89,19 +101,90 @@ class ClockPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     double centerX = size.width / 2;
     double centerY = size.height / 2;
-    double radius = min(centerX, centerY);
     Offset center = Offset(centerX, centerY);
+    double radius = min(centerX, centerY);
+    double outerRadius = radius - 20;
+    double innerRadius = radius - 30;
+
+    Paint hourDashPaint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 3
+      ..strokeCap = StrokeCap.round;
+
+    Paint secDashPaint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 3
+      ..strokeCap = StrokeCap.round;
+
+    for (int i = 0; i < 360; i += 30) {
+      double x1 = centerX - outerRadius * cos(i * pi / 180);
+      double y1 = centerX - outerRadius * sin(i * pi / 180);
+      double x2 = centerX - innerRadius * cos(i * pi / 180);
+      double y2 = centerX - innerRadius * sin(i * pi / 180);
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), hourDashPaint);
+    }
+
+    for (int i = 0; i < 360; i += 3) {
+      double x1 = centerX - outerRadius * .95 * cos(i * pi / 180);
+      double y1 = centerX - outerRadius * .95 * sin(i * pi / 180);
+      double x2 = centerX - innerRadius * cos(i * pi / 180);
+      double y2 = centerX - innerRadius * sin(i * pi / 180);
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), secDashPaint);
+    }
+
+    DateTime datetime = DateTime.now();
 
     Paint secLinePaint = Paint()
-      ..color = Colors.red
+      ..color = Color(0xFFE81466)
       ..strokeWidth = 4
       ..strokeCap = StrokeCap.round;
 
     Offset secStartOffset = Offset(
-      centerX - radius * cos(0 * pi / 180),
-      centerY - radius * cos(0 * pi / 180),
+      centerX + 20 * cos(datetime.second * 6 * pi / 180),
+      centerY + 20 * sin(datetime.second * 6 * pi / 180),
     );
-    canvas.drawLine(center, secStartOffset, secLinePaint);
+
+    Offset secEndOffset = Offset(
+      centerX - outerRadius * cos(datetime.second * 6 * pi / 180),
+      centerY - outerRadius * sin(datetime.second * 6 * pi / 180),
+    );
+
+    Paint minLinePaint = Paint()
+      ..color = Color(0xFFBEC5D5)
+      ..strokeWidth = 4
+      ..strokeCap = StrokeCap.round;
+
+    Offset minStartOffset = Offset(
+      centerX + 20 * cos(datetime.minute * 6 * pi / 180),
+      centerY + 20 * sin(datetime.minute * 6 * pi / 180),
+    );
+
+    Offset minEndOffset = Offset(
+      centerX - outerRadius * .6 * cos(datetime.minute * 6 * pi / 180),
+      centerY - outerRadius * .6 * sin(datetime.minute * 6 * pi / 180),
+    );
+
+    Paint hourLinePaint = Paint()
+      ..color = Color(0xFF222E63)
+      ..strokeWidth = 6
+      ..strokeCap = StrokeCap.round;
+
+    Offset hourEndOffset = Offset(
+      centerX - outerRadius * .4 * cos(datetime.hour * 6 * pi / 180),
+      centerY - outerRadius * .4 * sin(datetime.hour * 6 * pi / 180),
+    );
+
+    Offset hourStartOffset = Offset(
+      centerX + 20 * cos(datetime.hour * 6 * pi / 180),
+      centerY + 20 * sin(datetime.hour * 6 * pi / 180),
+    );
+
+    Paint centerCirclePaint = Paint()..color = Color(0xFFE81466);
+
+    canvas.drawLine(minStartOffset, minEndOffset, minLinePaint);
+    canvas.drawLine(hourStartOffset, hourEndOffset, hourLinePaint);
+    canvas.drawLine(secStartOffset, secEndOffset, secLinePaint);
+    canvas.drawCircle(center, 5, centerCirclePaint);
   }
 
   @override
